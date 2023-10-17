f602e2bb4c0d204da0941769452b80342eb5b3b3
cosmicCode42 <cosmicanswer@gmail.com>
2023-10-17 20:29:43 +0000

    cleanup

--- reduce.js
+++ reduce.js
@@ -4,14 +4,13 @@
  * command node reduce.js in the terminal
  */
 
-
 // Summing an array of numbers:
-
 const nums = [0, 1, 2, 3, 4, 5];
 
 let sum = nums.reduce((acc, curr) => acc + curr, 0);
-// console.log(sum);
+console.log(sum);
 
+// Working with objects
 const teamMembers = [
   {
     name: 'Andrew',
